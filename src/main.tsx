c95151b270357eb2e986f82405428f68f7403ec8
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-02-19 13:07:43 +0100

    ADD blan space

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
 // Erstelle das Root-Element und rendere die App
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement!);
